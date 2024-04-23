f3f9f5e09d711aabe901d11a6ee1acd0574ee4c9
Wojciech Z <wojtekz@aldec.com>
2024-04-23 12:38:06 +0200

    clean old tools

--- BSP/designs/libero2021.1/tysom_m_mpfs250t_ref_design_2/script_support/components/FabUART.tcl
+++
@@ -1,4 +0,0 @@
-# Exporting core FabUART to TCL
-# Exporting Create HDL core command for module FabUART
-create_hdl_core -file {hdl/FabUART.v} -module {FabUART} -library {work} -package {}
-# Exporting BIF information of  HDL core command for module FabUART
--- BSP/designs/libero2021.1/tysom_m_mpfs250t_ref_design_4/script_support/hdl_source.tcl
+++
@@ -1,2 +0,0 @@
-#Importing and Linking all the HDL source files used in the design
-import_files -library work -hdl_source $local_dir/script_support/hdl/data_unpacker.vhd
